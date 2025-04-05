3d38d7665288582cc639c428a1e94a04e44b3289
Avishka Priyasad <avishkapriyasad11@gmail.com>
2025-04-05 22:55:16 +0530

    initial commit

---
+++ src/main/java/com/avishka/ShinyResort/repo/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.avishka.ShinyResort.repo;
+
+import com.avishka.ShinyResort.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+}
---
+++ src/test/java/com/avishka/ShinyResort/ShinyResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avishka.ShinyResort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
